f2b0ecd45e0aec95db96fe4587f1401903af2880
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-30 22:46:56 +0530

    Bootstrped the project

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
 import express from "express"
-app.g
+import mongoose from "mongoose"
+import jwt from "jsonwebtoken"
+import zod from "zod"
+
+const app = express()
+
+app.post("/api/v1/signup",(req,res)=>{
+
+})  
+
+
+app.post("/api/v1/signin",(req,res)=>{
+
+})
+
